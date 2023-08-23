e2967f824589e986ddf14c94ca9c8cd3b5596438
shayan-shaikh <65915426+shayan-shaikh@users.noreply.github.com>
2023-08-23 22:36:06 +0530

    first commit with working logic

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
+
+  const handleAddNote = () => {
+    setNotes([...notes, '']);
+  };
+
+  const handleInputChange = (e, index) => {
+    const newNotes = [...notes];
+    newNotes[index] = e.target.value;
+    setNotes(newNotes);
+
+    if (e.target.value.includes('<> ')) {
+      setSuggestions(notes.slice(0, -1));
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  const handleSuggestionChange = (e) => {
+    const newNotes = [...notes];
+    newNotes[newNotes.length - 1] += e.target.value;
+    setNotes(newNotes);
+    setSuggestions([]);
+    if (newNotes[index].includes('<>')) {
+      setSuggestions(notes.slice(0, -1));
+    } else {
+      setSuggestions([]);
+      if (newNotes[index].includes('<>')) {
+        newNotes[index] = newNotes[index].replace('<>', '');
+        noteRefs.current[index].current.innerHTML = newNotes[index];
+      }
+    }
+  };
+
+  return (
+    <div style={{ backgroundColor: 'darkblue', padding: '20px' }}>
+      <button onClick={handleAddNote}>Create new note</button>
+      {notes.map((note, index) => (
+        <div key={index}>
+          <hr />
+          <textarea
+            value={note}
+            onChange={(e) => handleInputChange(e, index)}
+            style={{ backgroundColor: 'cyan' }}
+          />
+        </div>
+      ))}
+      {suggestions.length > 0 && (
+        <select onChange={handleSuggestionChange}>
+          <option value="">Select a suggestion</option>
+          {suggestions.map((suggestion, index) => (
+            <option key={index} value={suggestion}>
+              {suggestion}
+            </option>
+          ))}
+        </select>
+      )}
+    </div>
+  );
+};
+
+export default App;
+
+
