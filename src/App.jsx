67d1a51ffcf2f5085c510dbb07fd94faea02fdaf
shayan-shaikh <65915426+shayan-shaikh@users.noreply.github.com>
2023-08-25 22:54:58 +0530

    changes to styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+import "./App.css"
+import TodoInput from './components/TodoInput'
+import Todolist from './components/TodoList';
 
-const App = () => {
-  const [items, setItems] = useState([]);
-  const [suggestions, setSuggestions] = useState([]);
-  const [selectedSuggestionIndex, setSelectedSuggestionIndex] = useState(null);
 
-  const handleAddItem = () => {
-    setItems([...items, '']);
-  };
+function App() {
+  const [listTodo, setListTodo] = useState([]);
 
-  const handleInputChange = (e, index) => {
-    const newItems = [...items];
-    newItems[index] = e.target.value;
-    setItems(newItems);
-
-    if (e.target.value.includes('<>')) {
-      setSuggestions(newItems.slice(0, index));
-      setSelectedSuggestionIndex(index);
-    } else {
-      setSuggestions([]);
-      setSelectedSuggestionIndex(null);
-    }
-
-    if (e.target.value.endsWith('<>')) {
-      newItems[index] = newItems[index].slice(0, -2);
-      setItems(newItems);
-    }
-  };
-
-  const handleSuggestionClick = (suggestion) => {
-    const newItems = [...items];
-    newItems[selectedSuggestionIndex] = `${newItems[selectedSuggestionIndex]}${suggestion}`;
-    setItems(newItems);
-    setSuggestions([]);
-    setSelectedSuggestionIndex(null);
-  };
-
+  let addList = (inputText) => {
+    if (inputText !== '')
+      setListTodo([...listTodo, inputText]);
+  }
+  const deleteListItem = (key) => {
+    let newListTodo = [...listTodo];
+    newListTodo.splice(key, 1)
+    setListTodo([...newListTodo])
+  }
   return (
-    <div style={{ backgroundColor: 'darkblue', color: 'white', padding: '20px' }}>
-      <h1>Welcome to the assignment</h1>
-      <button onClick={handleAddItem}>Add Item</button>
-      {items.map((item, index) => (
-        <div key={index}>
-          <hr />
-          <input
-            type="text"
-            value={item}
-            onChange={(e) => handleInputChange(e, index)}
-            style={{ color: index === selectedSuggestionIndex ? 'red' : 'black' }}
-          />
-        </div>
-      ))}
-      {suggestions.length > 0 && (
-        <ul>
-          {suggestions.map((suggestion, index) => (
-            <li key={index} onClick={() => handleSuggestionClick(suggestion)}>
-              {suggestion}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
-};
 
-export default App;
+    <div className="main-container">
+      <div className="center-container">
+        <TodoInput addList={addList} listTodo={listTodo} />
+
+        <h1 className="app-heading">Todo List</h1>
+        <hr />
+        {listTodo.map((listItem, i) => {
+          return (
+            <Todolist key={i} index={i} item={listItem} deleteItem={deleteListItem} />
+          )
+        })}
+      </div>
+
+    </div>
+
+
+  )
+}
+
+export default App
---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "../components/todo.css";
+
+export default function TodoInput(props) {
+    const [inputText, setInputText] = useState('');
+    const [suggestions, setSuggestions] = useState([]);
+    const [showSuggestions, setShowSuggestions] = useState(false);
+
+    const handleInputChange = (event) => {
+        const newText = event.target.value;
+        setInputText(newText);
+
+        if (newText.endsWith('<> ')) {
+            const filteredSuggestions = props.listTodo;
+            setSuggestions(filteredSuggestions);
+            setShowSuggestions(true);
+        } else {
+            setSuggestions([]);
+            setShowSuggestions(false);
+        }
+    };
+
+    const handleSuggestionClick = (suggestion) => {
+        const updatedText = inputText + suggestion;
+        setInputText(updatedText);
+        setSuggestions([]);
+        setShowSuggestions(false);
+    };
+
+    return (
+
+        <div className="input-container">
+            <input
+                type="text"
+                className="input-box-todo"
+                placeholder="Enter any text"
+                value={inputText}
+                onChange={handleInputChange}
+            />
+            {showSuggestions && (
+                <ul className="suggestion-list">
+                    {suggestions.map((suggestionItem, index) => (
+                        <li
+                            key={index}
+                            onClick={() => handleSuggestionClick(suggestionItem)} updatedText={suggestions}
+                        >
+                            {suggestionItem}
+                        </li>
+                    ))}
+                </ul>
+            )}
+            <button
+                className="add-btn"
+                onClick={() => {
+                    props.addList(inputText);
+                    setInputText('');
+                }}
+            >
+                +
+            </button>
+        </div>
+    );
+}
